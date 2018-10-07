2599d7a3ba5077ec83075173b0b36c5fcfe49f8e
tfreese <commercial@freese-home.de>
2018-10-07 09:22:16 +0200

    module_info

---
+++ spring-thymeleaf/src/main/java/_module-info.java
@@ -0,0 +1,45 @@
+/// **
+// * @author Thomas Freese
+// */
+// module freese.spring.thymeleaf
+// {
+// opens de.freese.spring.thymeleaf;
+//
+// exports de.freese.spring.thymeleaf;
+// // exports de.freese.spring.thymeleaf.web;
+// exports de.freese.spring.thymeleaf.service;
+// exports de.freese.spring.thymeleaf.facade;
+// // exports de.freese.spring.thymeleaf.rest;
+// exports de.freese.spring.thymeleaf.config;
+// exports de.freese.spring.thymeleaf.model;
+// exports de.freese.spring.thymeleaf.exception;
+//
+// requires com.fasterxml.jackson.core;
+// requires com.fasterxml.jackson.databind;
+// requires commons.logging;
+// requires jackson.annotations;
+// requires java.annotation;
+// requires java.desktop;
+// requires java.management;
+// requires java.naming;
+// requires java.validation;
+// // requires junit;
+// requires jdk.incubator.httpclient;
+// requires org.apache.httpcomponents.httpclient;
+// requires org.apache.httpcomponents.httpcore;
+// requires org.hibernate.validator;
+// requires slf4j.api;
+// requires spring.aop;
+// requires spring.beans;
+// requires spring.boot;
+// requires spring.boot.autoconfigure;
+// requires spring.boot.devtools;
+// requires spring.context;
+// requires spring.core;
+// requires spring.security.config;
+// requires spring.security.core;
+// requires spring.security.web;
+// requires spring.web;
+// requires spring.webmvc;
+// requires tomcat.embed.core;
+// }
--- spring-thymeleaf/src/test/java/de/freese/spring/thymeleaf/AllThymeleafTests.java
+++ spring-thymeleaf/src/test/java/de/freese/spring/thymeleaf/AllThymeleafTests.java
@@ -6,7 +6,6 @@ package de.freese.spring.thymeleaf;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-import de.freese.spring.thymeleaf.rest.TestRestWithJreHttpClient;
 import de.freese.spring.thymeleaf.rest.TestRestWithMockMvc;
 import de.freese.spring.thymeleaf.rest.TestRestWithRestTemplate;
 import de.freese.spring.thymeleaf.rest.TestRestWithRestTemplateSSL;
@@ -27,8 +26,8 @@ import de.freese.spring.thymeleaf.web.TestWebApp;
         TestRestWithRestTemplate.class,
         TestRestWithRestTemplateSSL.class,
         TestRestWithWebClient.class,
-        TestRestWithWebClientSSL.class,
-        TestRestWithJreHttpClient.class
+        TestRestWithWebClientSSL.class
+        //, TestRestWithJreHttpClient.class
     // @formatter:off
 })
 public class AllThymeleafTests
