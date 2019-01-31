a09858c82f88c83b831df7fd16e413dbfb9f2308
tfreese <commercial@freese-home.de>
2019-01-31 18:30:33 +0100

    messaging with jms

---
+++ spring-messaging/spring-messaging-jms/src/main/java/de/freese/spring/messaging/jms/Email.java
@@ -0,0 +1,85 @@
+/**
+ * Created: 31.01.2019
+ */
+
+package de.freese.spring.messaging.jms;
+
+/**
+ * @author Thomas Freese
+ */
+public class Email
+{
+    /**
+     *
+     */
+    private String body = null;
+
+    /**
+     *
+     */
+    private String to = null;
+
+    /**
+     * Erstellt ein neues {@link Email} Object.
+     */
+    public Email()
+    {
+        super();
+
+    }
+
+    /**
+     * Erstellt ein neues {@link Email} Object.
+     *
+     * @param to String
+     * @param body String
+     */
+    public Email(final String to, final String body)
+    {
+        super();
+
+        this.to = to;
+        this.body = body;
+    }
+
+    /**
+     * @return String
+     */
+    public String getBody()
+    {
+        return this.body;
+    }
+
+    /**
+     * @return String
+     */
+    public String getTo()
+    {
+        return this.to;
+    }
+
+    /**
+     * @param body String
+     */
+    public void setBody(final String body)
+    {
+        this.body = body;
+    }
+
+    /**
+     * @param to String
+     */
+    public void setTo(final String to)
+    {
+        this.to = to;
+    }
+
+    /**
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString()
+    {
+        return String.format("Email{to=%s, body=%s}", getTo(), getBody());
+    }
+}
---
+++ spring-messaging/spring-messaging-jms/src/main/java/de/freese/spring/messaging/jms/SpringJmsApplication.java
@@ -0,0 +1,167 @@
+/**
+ * Created: 31.01.2019
+ */
+
+package de.freese.spring.messaging.jms;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+import javax.jms.ConnectionFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+import org.springframework.scheduling.concurrent.ThreadPoolExecutorFactoryBean;
+
+/**
+ * @author Thomas Freese
+ */
+@SpringBootApplication
+@EnableJms
+public class SpringJmsApplication
+{
+    /**
+     * @param args String[]
+     * @throws Exception Falls was schief geht.
+     */
+    public static void main(final String[] args) throws Exception
+    {
+        try (ConfigurableApplicationContext context = SpringApplication.run(SpringJmsApplication.class, args))
+        {
+            JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+
+            // Send a message with a POJO - the template reuse the message converter
+            for (int i = 0; i < 5; i++)
+            {
+                System.out.println("Sending an email message: " + (i + 1));
+                jmsTemplate.convertAndSend("mailbox", new Email("info@example.com", "Hello-" + (i + 1)));
+            }
+
+            Thread.sleep(1000);
+        }
+    }
+
+    /**
+     * @return {@link ThreadPoolExecutorFactoryBean}
+     */
+    @Bean
+    @Primary
+    public ThreadPoolExecutorFactoryBean executorService()
+    {
+        int coreSize = Runtime.getRuntime().availableProcessors() * 2;
+        int maxSize = coreSize * 2;
+        int queueSize = maxSize * 2;
+        int keepAliveSeconds = 60;
+
+        ThreadPoolExecutorFactoryBean bean = new ThreadPoolExecutorFactoryBean();
+        bean.setCorePoolSize(coreSize);
+        bean.setMaxPoolSize(maxSize);
+        bean.setQueueCapacity(queueSize);
+        bean.setKeepAliveSeconds(keepAliveSeconds);
+        bean.setThreadPriority(Thread.NORM_PRIORITY);
+        bean.setThreadNamePrefix("pool-");
+        bean.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        bean.setAllowCoreThreadTimeOut(false);
+        bean.setExposeUnconfigurableExecutor(true);
+
+        return bean;
+    }
+
+    /**
+     * Serialize message content to json using TextMessage
+     *
+     * @return {@link MessageConverter}
+     */
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter()
+    {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+
+        return converter;
+    }
+
+    /**
+     * @param connectionFactory {@link ConnectionFactory}
+     * @param configurer {@link DefaultJmsListenerContainerFactoryConfigurer}
+     * @param taskExecutor {@link Executor}
+     * @return {@link JmsListenerContainerFactory}
+     */
+    @Bean
+    public JmsListenerContainerFactory<?> myFactory(final ConnectionFactory connectionFactory, final DefaultJmsListenerContainerFactoryConfigurer configurer,
+                                                    final Executor taskExecutor)
+    {
+        // This provides all boot's default to this factory, including the message converter
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setTaskExecutor(taskExecutor);
+
+        // You could still override some of Boot's default if necessary.
+        configurer.configure(factory, connectionFactory);
+
+        return factory;
+    }
+
+    // /**
+    // * @return {@link ScheduledExecutorFactoryBean}
+    // */
+    // @Bean
+    // @ConditionalOnMissingBean(ScheduledExecutorService.class)
+    // public ScheduledExecutorFactoryBean scheduledExecutorService()
+    // {
+    // int poolSize = Runtime.getRuntime().availableProcessors();
+    //
+    // ScheduledExecutorFactoryBean bean = new ScheduledExecutorFactoryBean();
+    // bean.setPoolSize(poolSize);
+    // bean.setThreadPriority(Thread.NORM_PRIORITY);
+    // bean.setThreadNamePrefix("scheduler-");
+    // bean.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+    // bean.setExposeUnconfigurableExecutor(true);
+    //
+    // return bean;
+    // }
+    //
+    // /**
+    // * Wird für {@link EnableAsync} benötigt.
+    // *
+    // * @return {@link TaskExecutor}
+    // */
+    // @Bean(
+    // {
+    // "taskExecutor", "asyncTaskExecutor"
+    // })
+    // @ConditionalOnMissingBean(AsyncTaskExecutor.class)
+    // // public TaskExecutor springTaskExecutor(@Qualifier("executorService") final ExecutorService executorService)
+    // public AsyncTaskExecutor springTaskExecutor()
+    // {
+    // AsyncTaskExecutor bean = new ConcurrentTaskExecutor(executorService().getObject());
+    //
+    // return bean;
+    // }
+    //
+    // /**
+    // * Wird für {@link EnableScheduling} benötigt.
+    // *
+    // * @param executorService {@link ExecutorService}
+    // * @param scheduledExecutorService {@link ScheduledExecutorService}
+    // * @return {@link TaskScheduler}
+    // */
+    // @Bean("taskScheduler")
+    // @ConditionalOnMissingBean(TaskScheduler.class)
+    // public TaskScheduler springTaskScheduler(@Qualifier("executorService") final ExecutorService executorService,
+    // final ScheduledExecutorService scheduledExecutorService)
+    // {
+    // TaskScheduler bean = new ConcurrentTaskScheduler(executorService, scheduledExecutorService);
+    //
+    // return bean;
+    // }
+}
