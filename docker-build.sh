7fd8e45e6d9bdf2e470e4bd7ce924d17b58a574c
tfreese <commercial@freese-home.de>
2019-05-27 12:09:09 +0200

    docker

--- docker-build.sh
+++ docker-build.sh
@@ -11,12 +11,41 @@ mvn -f spring-eureka		clean package;
 mvn -f spring-microservice	clean package;
 mvn -f spring-resilience	clean package;
 
+# Image bauen
 docker build --tag=spring-boot-admin:1		spring-boot-admin;
 docker build --tag=spring-eureka:1			spring-eureka;
 docker build --tag=spring-microservice:1	spring-microservice;
 docker build --tag=spring-resilience:1		spring-resilience;
 
-docker tag spring-boot-admin:1		spring-boot-admin:latest;
-docker tag spring-eureka:1			spring-eureka:latest;
-docker tag spring-microservice:1	spring-microservice:latest;
-docker tag spring-resilience:1		spring-resilience:latest;
+# Version taggen
+docker tag spring-boot-admin:1			spring-boot-admin:latest;
+docker tag spring-eureka:1				spring-eureka:latest;
+docker tag spring-microservice:1		spring-microservice:latest;
+docker tag spring-resilience:1			spring-resilience:latest;
+
+# Für lokale Registry taggen
+docker tag spring-boot-admin:latest		localhost:5000/spring-boot-admin:latest;
+docker tag spring-eureka:latest			localhost:5000/spring-eureka:latest;
+docker tag spring-microservice:latest	localhost:5000/spring-microservice:latest;
+docker tag spring-resilience:latest		localhost:5000/spring-resilience:latest;
+
+# In die lokale Registry pushen
+docker push localhost:5000/spring-boot-admin:latest;
+docker push localhost:5000/spring-eureka:latest;
+docker push localhost:5000/spring-microservice:latest;
+docker push localhost:5000/spring-resilience:latest;
+
+# Lokale Images löschen
+docker image remove -f spring-boot-admin	spring-boot-admin:1		localhost:5000/spring-boot-admin:latest;
+docker image remove -f spring-eureka		spring-eureka:1			localhost:5000/spring-eureka:latest;
+docker image remove -f spring-microservice	spring-microservice:1	localhost:5000/spring-microservice:latest;
+docker image remove -f spring-resilience	spring-resilience:1		localhost:5000/spring-resilience:latest;
+
+# Image aus lokaler Registry neu laden
+docker pull localhost:5000/spring-boot-admin:latest;
+docker pull localhost:5000/spring-eureka:latest;
+docker pull localhost:5000/spring-microservice:latest;
+docker pull localhost:5000/spring-resilience:latest;
+
+
+
--- kubernetes/docker-build.sh
+++ kubernetes/docker-build.sh
@@ -5,9 +5,25 @@
 # docker run [-d] --name microservice -p 8080:8080 mymicroservice:1;
 # docker start/stop microservice;
 #
+
 mvn -f kubernetes-microservice clean package;
 
-docker build --tag=mymicroservice:1	kubernetes-microservice;
+# Image bauen
+docker build --tag=kubernetes-microservice:1	kubernetes-microservice;
 
-docker tag mymicroservice:1 mymicroservice:latest;
+# Version taggen
+docker tag kubernetes-microservice:1	kubernetes-microservice:latest;
+
+# Für lokale Registry taggen
+docker tag kubernetes-microservice:latest	localhost:5000/kubernetes-microservice:latest;
+
+# In die lokale Registry pushen
+docker push localhost:5000/kubernetes-microservice:latest;
+
+# Lokale Images löschen
+docker image remove -f kubernetes-microservice	kubernetes-microservice:1	localhost:5000/kubernetes-microservice:latest;
+
+# Image aus lokaler Registry neu laden
+docker pull localhost:5000/kubernetes-microservice:latest;
+
 
