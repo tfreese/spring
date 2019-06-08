de58e13df0b0485b725a88e9e94813e291c3a4a3
tfreese <commercial@freese-home.de>
2019-06-08 10:21:05 +0200

    kubernetes

--- kubernetes/kubernetes-deploy.sh
+++ kubernetes/kubernetes-deploy.sh
@@ -3,7 +3,7 @@
 # Für lokales Images: 
 # eval $(minikube docker-env);
 
-kubectl run microservice --image=mymicroservice:latest --port=8080 --image-pull-policy=Never
+kubectl run microservice --image=kubernetes-microservice:latest --port=8080 --image-pull-policy=Never
 kubectl expose deployment/microservice --type="LoadBalancer" --port 8080
 
 #kubectl run hello-minikube --image=k8s.gcr.io/echoserver:1.10 --port=8080;
