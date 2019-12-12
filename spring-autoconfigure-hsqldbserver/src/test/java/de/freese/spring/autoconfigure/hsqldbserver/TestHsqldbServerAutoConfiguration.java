ec7b5b8aadaddc2b3c71a542dd9686125617aad4
tfreese <commercial@freese-home.de>
2019-12-12 18:35:36 +0100

    polish

--- spring-autoconfigure-hsqldbserver/src/test/java/de/freese/spring/autoconfigure/hsqldbserver/TestHsqldbServerAutoConfiguration.java
+++ spring-autoconfigure-hsqldbserver/src/test/java/de/freese/spring/autoconfigure/hsqldbserver/TestHsqldbServerAutoConfiguration.java
@@ -14,22 +14,22 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import javax.annotation.Resource;
 import javax.sql.DataSource;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.util.SocketUtils;
 
 /**
  * @author Thomas Freese
  */
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
+@TestMethodOrder(MethodOrderer.Alphanumeric.class)
 @SpringBootTest(classes = TestApplication.class)
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestHsqldbServerAutoConfiguration
 {
     static
--- spring-oauth2/spring-oauth2-authorisationserver/src/test/java/de/freese/spring/oauth2/authorisation/OAuth2AuthorisationServerTests.java
+++ spring-oauth2/spring-oauth2-authorisationserver/src/test/java/de/freese/spring/oauth2/authorisation/OAuth2AuthorisationServerTests.java
@@ -4,16 +4,19 @@
 
 package de.freese.spring.oauth2.authorisation;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * @author Thomas Freese
  */
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
+@TestMethodOrder(MethodOrderer.Alphanumeric.class)
 @SpringBootTest
 @ActiveProfiles("test, memory")
 public class OAuth2AuthorisationServerTests
