111d7d677189264e99ab98ca679baf1ebfb9b250
tfreese <commercial@freese-home.de>
2021-04-12 18:35:23 +0200

    polish

--- spring-resilience/src/test/java/de/freese/spring/resilience/TestResilience.java
+++ spring-resilience/src/test/java/de/freese/spring/resilience/TestResilience.java
@@ -5,11 +5,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.Duration;
 import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.github.resilience4j.bulkhead.Bulkhead;
+import io.github.resilience4j.bulkhead.BulkheadConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.decorators.Decorators;
 import io.github.resilience4j.retry.Retry;
 import io.github.resilience4j.retry.Retry.Metrics;
 import io.github.resilience4j.retry.RetryConfig;
@@ -23,6 +30,70 @@ class TestResilience
      *
      */
     private static final Logger LOGGER = LoggerFactory.getLogger(TestResilience.class);
+
+    /**
+     * @throws Exception Falls was schief geht.
+     */
+    @Test
+    void testDecorators() throws Exception
+    {
+        Object data = new Object();
+
+        Callable<Object> failingCode = () -> {
+            double value = Math.random();
+
+            if (value < 0.5D)
+            {
+                LOGGER.info("throw Exception: {}", value);
+                throw new Exception("test exception");
+            }
+
+            LOGGER.info("return data: {}", value);
+            return data;
+        };
+
+        // CircuitBreaker circuitBreaker = CircuitBreaker.ofDefaults("backendService");
+        CircuitBreaker circuitBreaker = CircuitBreaker.of("backendService", CircuitBreakerConfig.custom().failureRateThreshold(50F).build());
+        circuitBreaker.getEventPublisher().onError(event -> LOGGER.error(event.toString()));
+
+        // Retry retry = Retry.ofDefaults("backendService");
+        Retry retry = Retry.of("backendService", RetryConfig.custom().maxAttempts(10).waitDuration(Duration.ofMillis(100)).build());
+        retry.getEventPublisher().onRetry(event -> LOGGER.info(event.toString()));
+        retry.getEventPublisher().onSuccess(event -> LOGGER.info(event.toString()));
+
+        // Bulkhead bulkhead = Bulkhead.ofDefaults("backendService");
+        Bulkhead bulkhead = Bulkhead.of("backendService", BulkheadConfig.custom().maxConcurrentCalls(5).maxWaitDuration(Duration.ofMillis(10)).build());
+        bulkhead.getEventPublisher().onCallRejected(event -> LOGGER.error(event.toString()));
+
+        // @formatter:off
+        Callable<Object> decoratedSupplier = Decorators.ofCallable(failingCode)
+                .withCircuitBreaker(circuitBreaker)//.withFallback(th -> data)
+                .withRetry(retry)
+                .withBulkhead(bulkhead)
+                .decorate()
+                ;
+        // @formatter:on
+
+        AtomicReference<Object> valueReference = new AtomicReference<>();
+
+        IntStream.range(0, 10).parallel().forEach(i -> {
+            try
+            {
+                Object value = decoratedSupplier.call();
+
+                if (value != null)
+                {
+                    valueReference.set(value);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Ignore
+            }
+        });
+
+        assertEquals(data, valueReference.get());
+    }
 
     /**
      * @throws Exception Falls was schief geht.
