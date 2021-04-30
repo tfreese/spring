828e96c7d0bb62fc2d0d7ae5806d344fecc886e7
tfreese <commercial@freese-home.de>
2021-04-30 18:37:41 +0200

    generateChainOfTrust

--- spring-thymeleaf/CA/generateChainOfTrust.sh
+++ spring-thymeleaf/CA/generateChainOfTrust.sh
@@ -18,9 +18,9 @@ echo "Erzeugen eines self signed key pair root CA Zertifikats.";
 echo "####################################################################################################";
 keytool -genkey -v \
   -storetype PKCS12 \
-  -keystore tommy_ca.p12 \
+  -keystore root_ca.p12 \
   -storepass "$PW" \
-  -alias tommy_ca_1 \
+  -alias root_ca_1 \
   -dname "CN=Thomas Freese: Root CA 1, OU=Development, O=Thomas Freese, L=Braunschweig, ST=Niedersachsen, C=DE" \
   -keyalg RSA \
   -keysize 4096 \
@@ -33,47 +33,87 @@ echo "##########################################################################
 echo "Exportieren des CA public Zertifikats als *.crt so das es in TrustStores verwendet werden kann.";
 echo "####################################################################################################";
 keytool -export -v \
-  -keystore tommy_ca.p12 \
+  -keystore root_ca.p12 \
   -storepass "$PW" \
-  -alias tommy_ca_1 \
-  -file tommy_ca_1_pub.crt \
+  -alias root_ca_1 \
+  -file root_ca_1_pub.crt \
   -rfc;
 
 echo;
 echo "####################################################################################################";
-echo "Erzeugen eines Server Zertifikats, z.B. localhost.";
+echo "Erzeugen eines Server Zertifikats, z.B. server.";
 echo "####################################################################################################";
 keytool -genkey -v \
   -storetype PKCS12 \
   -keystore server_keystore.p12 \
   -storepass "$PW" \
-  -alias localhost \
-  -dname "CN=Thomas Freese: localhost, OU=Development, O=Thomas Freese, L=Braunschweig, ST=Niedersachsen, C=DE" \
+  -alias server \
+  -dname "CN=Thomas Freese: server, OU=Development, O=Thomas Freese, L=Braunschweig, ST=Niedersachsen, C=DE" \
   -keyalg RSA \
-  -keysize 2048 \
+  -keysize 4096 \
+  -validity 36500;
+  
+echo;
+echo "####################################################################################################";
+echo "Erzeugen eines Client Zertifikats, z.B. client.";
+echo "####################################################################################################";
+keytool -genkey -v \
+  -storetype PKCS12 \
+  -keystore client_keystore.p12 \
+  -storepass "$PW" \
+  -alias client \
+  -dname "CN=Thomas Freese: client, OU=Development, O=Thomas Freese, L=Braunschweig, ST=Niedersachsen, C=DE" \
+  -keyalg RSA \
+  -keysize 4096 \
   -validity 36500;
 
 echo;
 echo "####################################################################################################";
-echo "Erzeugen eines Certificate Signing Requests (CSR) für localhost.";
+echo "Erzeugen eines Certificate Signing Requests (CSR) für server.";
 echo "####################################################################################################";
 keytool -certreq -v \
   -keystore server_keystore.p12 \
   -storepass "$PW" \
-  -alias localhost \
-  -file localhost.csr;
+  -alias server \
+  -file server.csr;
 
 echo;
 echo "####################################################################################################";
-echo "Signieren des localhost-Zertifikats und des -CSR mit dem ROOT CA Zertifikat.";
+echo "Erzeugen eines Certificate Signing Requests (CSR) für client.";
+echo "####################################################################################################";
+keytool -certreq -v \
+  -keystore client_keystore.p12 \
+  -storepass "$PW" \
+  -alias client \
+  -file client.csr;
+
+echo;
+echo "####################################################################################################";
+echo "Signieren des server-Zertifikats und des -CSR mit dem ROOT CA Zertifikat.";
 echo "KeyEncipherment for RSA: DHE or ECDHE.";
 echo "####################################################################################################";
 keytool -gencert -v \
-  -keystore tommy_ca.p12 \
+  -keystore root_ca.p12 \
   -storepass "$PW" \
-  -alias tommy_ca_1 \
-  -infile localhost.csr \
-  -outfile localhost.crt \
+  -alias root_ca_1 \
+  -infile server.csr \
+  -outfile server.crt \
+  -ext KeyUsage:critical="digitalSignature,keyEncipherment" \
+  -ext EKU="serverAuth" \
+  -ext SAN="DNS:localhost" \
+  -rfc;
+
+echo;
+echo "####################################################################################################";
+echo "Signieren des client-Zertifikats und des -CSR mit dem ROOT CA Zertifikat.";
+echo "KeyEncipherment for RSA: DHE or ECDHE.";
+echo "####################################################################################################";
+keytool -gencert -v \
+  -keystore root_ca.p12 \
+  -storepass "$PW" \
+  -alias root_ca_1 \
+  -infile client.csr \
+  -outfile client.crt \
   -ext KeyUsage:critical="digitalSignature,keyEncipherment" \
   -ext EKU="serverAuth" \
   -ext SAN="DNS:localhost" \
@@ -84,11 +124,22 @@ echo "##########################################################################
 echo "Import des ROOT CA public Zertifikats in den Server TrustStore.";
 echo "####################################################################################################";
 keytool -import -v \
-  -storetype PKCS12 \
   -keystore server_truststore.p12 \
   -storepass "$PW" \
-  -alias tommy_ca_1 \
-  -file tommy_ca_1_pub.crt << EOF
+  -alias root_ca_1 \
+  -file root_ca_1_pub.crt << EOF
+ja
+EOF
+
+echo;
+echo "####################################################################################################";
+echo "Import des ROOT CA public Zertifikats in den Client TrustStore.";
+echo "####################################################################################################";
+keytool -import -v \
+  -keystore client_truststore.p12 \
+  -storepass "$PW" \
+  -alias root_ca_1 \
+  -file root_ca_1_pub.crt << EOF
 ja
 EOF
 
@@ -99,35 +150,71 @@ echo "##########################################################################
 keytool -import -v \
   -keystore server_keystore.p12 \
   -storepass "$PW" \
-  -alias tommy_ca_1 \
-  -file tommy_ca_1_pub.crt \
-  -storetype JKS << EOF
+  -alias root_ca_1 \
+  -file root_ca_1_pub.crt << EOF
 ja
 EOF
 
 echo;
 echo "####################################################################################################";
-echo "Import des signierten localhost-Zertifikats in den Server KeyStore, das alte unsignierte wird dabei überschrieben.";
+echo "Import des ROOT CA public Zertifikats in den Client KeyStore.";
+echo "####################################################################################################";
+keytool -import -v \
+  -keystore client_keystore.p12 \
+  -storepass "$PW" \
+  -alias root_ca_1 \
+  -file root_ca_1_pub.crt << EOF
+ja
+EOF
+
+echo;
+echo "####################################################################################################";
+echo "Import des signierten Server-Zertifikats in den Server KeyStore, das alte unsignierte wird dabei überschrieben.";
 echo "####################################################################################################";
 keytool -import -v \
   -keystore server_keystore.p12 \
   -storepass "$PW" \
-  -alias localhost \
-  -file localhost.crt \
-  -storetype JKS;
+  -alias server \
+  -file server.crt;
+  
+echo;
+echo "####################################################################################################";
+echo "Import des signierten Client-Zertifikats in den Client KeyStore, das alte unsignierte wird dabei überschrieben.";
+echo "####################################################################################################";
+keytool -import -v \
+  -keystore client_keystore.p12 \
+  -storepass "$PW" \
+  -alias client \
+  -file client.crt;
 
 echo;
+echo "####################################################################################################";
+echo "Inhalt Server-Keystore";
 echo "####################################################################################################";
 keytool -list \
   -keystore server_keystore.p12 \
   -storepass "$PW";
-  
+
 echo;
 echo "####################################################################################################";
-echo "Inhalt des Server KeyStore";
+echo "Inhalt Client-Keystore";
 echo "####################################################################################################";
-keytool -list -v \
-  -keystore server_keystore.p12 \
+keytool -list \
+  -keystore client_keystore.p12 \
   -storepass "$PW";
 
+echo;
+echo "####################################################################################################";
+echo "Inhalt des Server TrustStore";
+echo "####################################################################################################";
+keytool -list -v \
+  -keystore server_truststore.p12 \
+  -storepass "$PW";
 
+echo;
+echo "####################################################################################################";
+echo "Inhalt des Client TrustStore";
+echo "####################################################################################################";
+keytool -list -v \
+  -keystore client_truststore.p12 \
+  -storepass "$PW"
