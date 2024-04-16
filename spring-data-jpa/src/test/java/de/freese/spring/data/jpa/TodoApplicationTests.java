ce3a4580cdce4405327a91ad75b5198c4332b23e
tfreese <community@freese-home.de>
2024-04-16 18:04:42 +0200

    jpa: WebConfig

--- spring-data-jpa/src/test/java/de/freese/spring/data/jpa/TodoApplicationTests.java
+++ spring-data-jpa/src/test/java/de/freese/spring/data/jpa/TodoApplicationTests.java
@@ -26,10 +26,21 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.web.client.RestClientCustomizer;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.JdkClientHttpConnector;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.reactive.server.WebTestClientConfigurer;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -49,6 +60,7 @@ import de.freese.spring.data.jpa.exception.ApplicationException;
  */
 @SpringBootTest(properties = "server.port=0", webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@Import(WebConfig.class)
 class TodoApplicationTests {
 
     @LocalServerPort
@@ -163,15 +175,19 @@ class TodoApplicationTests {
         final RestClient restClient = restClientBuilder.baseUrl("http://localhost:" + localServerPort).build();
 
         final HttpClientErrorException exception = assertThrows(HttpClientErrorException.NotFound.class,
-                () -> restClient.get().uri("/api/todo/" + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON).retrieve()
-                        //                        .onStatus(new DefaultResponseErrorHandler() {
-                        //                            @Override
-                        //                            public boolean hasError(final ClientHttpResponse response) throws IOException {
-                        //                                return false;
-                        //                            }
-                        //                        })
+                () -> restClient.get()
+                        .uri("/api/todo/" + UUID.randomUUID())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .retrieve()
+                        // .onStatus(new DefaultResponseErrorHandler() {
+                        //     @Override
+                        //     public boolean hasError(final ClientHttpResponse response) throws IOException {
+                        //         return false;
+                        //     }
+                        // })
                         .body(Todo.class));
 
+        assertNotNull(exception);
         assertEquals(HttpStatus.NOT_FOUND.value(), exception.getStatusCode().value());
 
         // final ProblemDetail problemDetail = exception.getResponseBodyAs(ProblemDetail.class);
@@ -246,5 +262,38 @@ class TodoApplicationTests {
                     System.out.println(value);
                     assertNotNull(value);
                 });
+    }
+}
+
+@Configuration
+class WebConfig {
+    @Bean
+    public ClientHttpConnector clientHttpConnector() {
+        return new JdkClientHttpConnector();
+    }
+
+    @Bean
+    public ClientHttpRequestFactory clientHttpRequestFactory() {
+        return new JdkClientHttpRequestFactory();
+    }
+
+    @Bean
+    RestClientCustomizer restClientCustomizer(final ClientHttpRequestFactory clientHttpRequestFactory) {
+        return restClientBuilder -> restClientBuilder.requestFactory(clientHttpRequestFactory);
+    }
+
+    @Bean
+    RestTemplateCustomizer restTemplateCustomizer(final ClientHttpRequestFactory clientHttpRequestFactory) {
+        return restTemplate -> restTemplate.setRequestFactory(clientHttpRequestFactory);
+    }
+
+    @Bean
+    WebClientCustomizer webClientCustomizer(final ClientHttpConnector clientHttpConnector) {
+        return webClientBuilder -> webClientBuilder.clientConnector(clientHttpConnector);
+    }
+
+    @Bean
+    WebTestClientConfigurer webTestClientConfigurer(final ClientHttpConnector clientHttpConnector) {
+        return (builder, httpHandlerBuilder, connector) -> builder.clientConnector(clientHttpConnector);
     }
 }
