988495667e672d6b76607ceb0ca8a0ad5d29c0a5
tfreese <community@freese-home.de>
2025-06-10 16:54:55 +0200

    spring-ai-demo

---
+++ spring-ai-demo/src/main/java/com/example/spring_ai_demo/SpringAiDemoApplication.java
@@ -0,0 +1,11 @@
+package com.example.spring_ai_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiDemoApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(SpringAiDemoApplication.class, args);
+    }
+}
