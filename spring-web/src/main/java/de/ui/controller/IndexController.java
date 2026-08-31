0a50e447bc78935b07dd7635575068180f991bf4
tfreese <community@freese-home.de>
2026-08-31 18:01:34 +0200

    polish

--- spring-web/src/main/java/de/ui/controller/IndexController.java
+++ spring-web/src/main/java/de/ui/controller/IndexController.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * @author Thomas Freese
@@ -43,6 +44,11 @@ public class IndexController {
 
     public User getUser() {
         return user;
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/persons.xhtml";
     }
 
     public void initParams() {
